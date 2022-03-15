9c05c54429972f119670ff8419570a2570b20310
hemanta <hkp003@gmail.com>
2022-03-15 16:58:42 +0530

    set home page as index page

--- page.php
+++ page.php
@@ -3,7 +3,7 @@ get_header();
 ?>
 
 <div class="about-page" style="border:2px solid red ; height:500px; width:100%;" >
-<h1><?php the_title() ?>:</h1>
+<h1><?php the_title(); ?></h1>
 </div>
 <?php
 get_footer();
