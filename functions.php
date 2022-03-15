3882173a57d518e31b1a3c5a59947a85b3ef8068
hemanta <hkp003@gmail.com>
2022-03-15 18:58:19 +0530

    added thumbnail image

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 register_nav_menus(
     array('primary-menu'=>'top Menu')
-)
+);
+
+add_theme_support('post-thumbnails');
 ?>
--- page.php
+++ page.php
@@ -1,10 +1,20 @@
 <?php
 get_header();
+// it was used in older versions
+the_post();
 ?>
 
-<div class="about-page" style="border:2px solid red ; height:500px; width:100%;" >
-<h1><?php the_title(); ?></h1>
+<div class="about-page" style="border:2px solid red ; height:auto; width:100%;" >
+<h1><?php the_title(); ?> :</h1>
+<?php the_post_thumbnail(array('300,300')); ?>
+<?php the_content(); ?>
+
+
+
+
 </div>
+
+
 <?php
 get_footer();
 ?>
