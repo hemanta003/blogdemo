18e3b9cf08e8e2d8972f68eef53f7ca79174d8f9
hemanta <hkp003@gmail.com>
2022-03-15 13:11:29 +0530

    added header and footer function

---
+++ footer.php
@@ -0,0 +1,76 @@
+
+
+<footer class="bg-dark text-white pt-5 pb-4">
+    <div class="footerbox text-center text-md-left">
+      <div class="row text-center text-md-left">
+        <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+          <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Company Name</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi iure quia dolor dolorem unde nemo?</p>
+        </div>
+
+
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+          <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Products </h5>
+          <p><a href="#" class="text-white" style="text-decoration: none;">The Providers</a></p>
+          <p><a href="#" class="text-white" style="text-decoration: none;">Creativity</a></p>
+          <p><a href="#" class="text-white" style="text-decoration: none;">Sourcefile</a></p>
+          <p><a href="#" class="text-white" style="text-decoration: none;">Bootstrap5 link</a></p>
+        </div>
+
+
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+          <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Links</h5>
+          <p><a href="#" class="text-white" style="text-decoration: none;">Facebook</a></p>
+          <p><a href="#" class="text-white" style="text-decoration: none;">Instagram</a></p>
+          <p><a href="#" class="text-white" style="text-decoration: none;">twitter</a></p>
+          <p><a href="#" class="text-white" style="text-decoration: none;">Email</a></p>
+        </div>
+        <div class="col-md-4 col-lg-2 col-xl-2 mx-auto mt-3">
+          <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Contact Us</h5>
+          <p>
+            <i class="fas fa-home mr-3"></i>Gtcsys,b205,CorporateRoad, profitaire A,Ahmedabad
+          </p>
+          <p>
+            <i class="fas fa-envelope mr-3"></i>gtcsys_help@gmail.com
+          </p>
+          <p>
+            <i class="fas fa-phone mr-3"></i>+91 9876543423
+          </p>
+          <p>
+            <i class="fas fa-print mr-3"></i>+91 654352829
+          </p>
+        </div>
+        <hr class="mb-4">
+        <div class="row align-items-center">
+          <div class="col-md-7 col-lg-8">
+            <p>Copyright @2022 All Rights Reserved By: <a href="#" style="text-decoration: none;"> <strong
+                  class="text-warning">The Providers</strong></a></p>
+          </div>
+          <div class="col-md-5 col-lg-4">
+                <div class="text-center text-md-right">
+                      <ul class="list-unstyled list-inline">
+                          <li class="list-inline-item">
+                              <a href="#" class="btn-floating btn-sm " style="font-size: 23px;"><i class="fab fa-facebook "></i></a>
+
+                          </li>
+
+                          <li class="list-inline-item">
+                            <a href="#" class="btn-floating btn-sm " style="font-size: 23px;"><i class="fab fa-instagram "></i></a>
+
+                        </li>
+                        <li class="list-inline-item">
+                          <a href="#" class="btn-floating btn-sm " style="font-size: 23px;"><i class="fab fa-twitter "></i></a>
+
+                      </li>
+                      <li class="list-inline-item">
+                        <a href="#" class="btn-floating btn-sm " style="font-size: 23px;"><i class="fab fa-youtube "></i></a>
+
+                    </li>
+                      </ul>
+                </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+
