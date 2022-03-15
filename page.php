8240c231813d95e3ce079088e8a5f05e2086dcd6
hemanta <hkp003@gmail.com>
2022-03-15 16:33:53 +0530

    added page templateusing the title function

--- page.php
+++ page.php
@@ -1,1 +1,10 @@
-<header class="php"></header>
+<?php
+get_header();
+?>
+
+<div class="about-page" style="border:2px solid red ; height:500px; width:100%;" >
+<h1><?php the_title() ?>:</h1>
+</div>
+<?php
+get_footer();
+?>
