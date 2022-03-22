81f8d623ceb68cddea9a7f6fb60d49c5d71540d7
hemanta <hkp003@gmail.com>
2022-03-22 12:16:29 +0530

    dynamic title of the website

--- functions.php
+++ functions.php
@@ -5,4 +5,16 @@ register_nav_menus(
 
 add_theme_support('post-thumbnails');
 add_theme_support('custom-header');
+
+register_sidebar(
+array(
+    'name'=>'Sidebar Location',
+    'id'=>'sidebar'    
+)
+
+)
+
+
+
+
 ?>
--- header.php
+++ header.php
@@ -11,7 +11,9 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>khateraho</title>
+  <title><?php bloginfo('name'); ?>
+    <?php wp_title(); ?>  |
+    <?php if (is_front_page()){echo "|"; bloginfo('description');} ?></title>
   <?php wp_head(); ?>
 </head>
 
